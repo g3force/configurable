9516cc25f311a12bbdb43f31a4516d4539438cc7
Nicolai Ommer <nicolai.ommer@gmail.com>
2015-11-25 23:02:05 +0100

    fixed bug: ambiguous class names conflicting with package names

--- src/main/java/com/github/g3force/configurable/ConfigAnnotationProcessor.java
+++ src/main/java/com/github/g3force/configurable/ConfigAnnotationProcessor.java
@@ -129,13 +129,13 @@ public class ConfigAnnotationProcessor
 					}
 				} catch (NoSuchFieldException e)
 				{
-					log.info("Field vanished: " + className + "#" + fieldName);
+					log.info("Field vanished in " + category + ": " + className + "#" + fieldName);
 				} catch (SecurityException e)
 				{
 					log.error("Sec exception", e);
 				} catch (ClassNotFoundException e)
 				{
-					log.info("Class vanished: " + className);
+					log.info("Class vanished in " + category + ": " + className);
 				}
 			}
 		}
@@ -393,7 +393,7 @@ public class ConfigAnnotationProcessor
 		{
 			Class.forName(base);
 			classes.put(base, node);
-		} catch (ClassNotFoundException err)
+		} catch (ClassNotFoundException | NoClassDefFoundError err)
 		{
 			// not a class. Maybe a package
 			for (ConfigurationNode child : node.getChildren())
